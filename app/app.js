09ed6b41d44c9b556be904bf09563d0b7d823e90
Arnab Saha <72820989+ArnabTo@users.noreply.github.com>
2021-10-03 14:52:12 +0600

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,26 +1,13 @@
-document.addEventListener("mousemove", function(event) {
-  const x = event.pageX - 10;
-  const y = event.pageY - 10;
-  const cursor = document.querySelector("#cursor");
-  cursor.style.left = x + "px";
-  cursor.style.top = y + "px";
-});
+const cursor = document.querySelector('#cursor')
+document.addEventListener('mousemove', (e) =>{
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+})
 
-$('h2,h3,p,i,a').on("mouseover", function(){
+$('h1,h2,h3,p,i,a').on("mouseover", function(){
   $('#cursor').css({'transform': 'scale(3)'});
 });
 
-$('h2,h3,p,i,a').on("mouseleave", function(){
+$('h1,h2,h3,p,i,a').on("mouseleave", function(){
   $('#cursor').css({'transform': 'scale(2)'});
 });
-
-$('h1').on("mouseover", function(){
-  $('#cursor').css({'transform': 'scale(4)'});
-});
-$('h1').on("mouseleave", function(){
-  $('#cursor').css({'transform': 'scale(3)'});
-});
-
-// sayHello
-var Hello = ["\n%c STOP RIGHT THERE CRIMINAL SCUM!" + " %c http://www.nørman.com/ \n", "color: white; font-size: 14px; background: #000; padding:5px 0;", "background: #fff; padding:3px 0; margin-bottom: 4px;"];
-window.console.log.apply(console, Hello);
